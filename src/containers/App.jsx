5f7ab21882da84206f2debe5d1b934fb509d7bdf
Luis Ventura <luisigsystem@gmail.com>
2019-08-23 16:10:18 -0500

    get data api reacthooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,8 +9,13 @@ import Education from '../components/Education'
 import Experience from '../components/Experience'
 import Certificates from '../components/Certificates'
 import Skills from '../components/Skills'
+// Importando ReactHooks para obtener data
+import useGetData from '../hooks/useGetData'
 
 const App = () => {
+    const data = useGetData()
+    console.log(data)
+
     return(
         <Main>
             <Sidebar>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
-import TestHooks from './components/TestHooks'
 
-// ReactDOM.render(<App />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
-ReactDOM.render(<TestHooks />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
+ReactDOM.render(<App />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
