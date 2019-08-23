daf6b9e660f070d68d939f90863cc842cd449f44
Luis Ventura <luisigsystem@gmail.com>
2019-08-23 14:46:12 -0500

    test ReactHooks using states

---
+++ src/components/TestHooks.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class TestHooks extends Component {
+    state = {
+        active: true,
+    }
+
+    handleClick = () => {
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.handleClick} >Ocultar</button>                
+                {this.state.active &&
+                    <h1>Hola Mundo!</h1>
+                }
+            </div>
+        )
+    }
+}
+
+export default TestHooks
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
+import TestHooks from './components/TestHooks'
 
-ReactDOM.render(<App />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
+// ReactDOM.render(<App />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
+ReactDOM.render(<TestHooks />, document.getElementById('app')) // render('Que vamos a empujar','Dónde lo vamos a empujar')
